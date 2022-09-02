2716d7447566a1ab0d4361bf9904abde3d9de178
LK <lunakiira0911@gmail.com>
2022-09-02 17:00:21 -0400

    added the renaming roles, dept

--- server.js
+++ server.js
@@ -3,22 +3,29 @@ const inquirer = require("inquirer");
 
 const startMenu = {
   name: "functionality",
-  message: "Hello, welcome employee manager, what would you like to do today!!?",
+  message:
+    "Hello, welcome employee manager, what would you like to do today!!?",
   type: "list",
   choices: [
     "Add Employee",
+    "Add Department",
+    "Add Roles",
     "Update Employee",
     "Show All Employees",
-    "Delete an Employee",
+    "Show All Departments",
+    "Show All Roles",
+    "Exit",
   ],
 };
 
 const showAllEmployees = () => {
   //make a call to the db, and show all employees
-  db.query(`SELECT e1.id as EMP_ID, CONCAT(e1.first_name, ' ', e1.last_name) as Name, title as role, 
+  db.query(
+    `SELECT e1.id as EMP_ID, CONCAT(e1.first_name, ' ', e1.last_name) as Name, title as role, 
   salary, department.name as department, IFNULL(CONCAT(e2.first_name, ' ', e2.last_name), 'No Manager, Bawss Status') as Manager FROM employee e1 LEFT JOIN role 
   ON e1.role_id=role.id LEFT JOIN department ON role.department_id=department.id
-  LEFT JOIN employee e2 ON e1.manager_id=e2.id `).then((results) => {
+  LEFT JOIN employee e2 ON e1.manager_id=e2.id `
+  ).then((results) => {
     console.log("--------------  EMPLOYEES  --------------");
     console.table(results);
     console.log("--------------  EMPLOYEES  --------------");
@@ -85,6 +92,68 @@ const addEmployee = () => {
   );
 };
 
+const addDepartment = () => {
+  db.query(`SELECT id, name FROM department`).then((departments) => {
+    // const departmentChoices = department.map((dept) => dept)
+
+    //convert my  results to a array of choices 4 inquirer prompt
+    const addDepartmentPrompt = [
+      {
+        name: "name",
+        message: "What is your Department?",
+        type: "input",
+        validate: (res) => !departments.includes(res.name),
+      },
+    ];
+
+    inquirer.prompt(addDepartmentPrompt).then((results) => {
+      console.log("RESULTS --- ", results);
+
+      db.query("INSERT INTO department SET ?", results).then(() => {
+        console.log("Department Successfully added to database");
+        setTimeout(start, 3000);
+      });
+    });
+  });
+};
+
+const addRole = () => {
+  db.query(`SELECT id, title FROM role`).then((role) => {
+    db.query(`SELECT id, name FROM dep`).then((deps) =>{
+    //convert my  results to a array of choices 4 inquirer prompt
+    const addRolePrompt = [
+      {
+        name: "title",
+        message: "What is your title?",
+        type: "input",
+        validate: (res) => !role.includes(res.title),
+      },
+      {
+        name: "salary", 
+        message: "What is your salary?",
+        type: "input", 
+      },
+
+      {
+        name: "department_id",
+        message: "Who is your department ID?",
+        type: "list",
+        choices: deps.map(dep => ({name: dep.title, value: dep.id})),
+      },
+    ];
+
+    inquirer.prompt(addRolePrompt).then((results) => {
+      console.log("RESULTS --- ", results);
+
+      db.query("INSERT INTO Role SET ?", results).then(() => {
+        console.log("Role Successfully added to database");
+        setTimeout(start, 3000);
+      });
+    });
+  })
+  });
+};
+
 function start() {
   inquirer.prompt(startMenu).then((response) => {
     console.log(response);
@@ -97,20 +166,19 @@ function start() {
         return addEmployee();
 
       case "Update Employee Role":
-        return updateEmployeeRole();
+        return updateEmployee();
 
       case "View All Departments":
         return viewAllDepartments();
 
-      case "Add Department" :
+      case "Add Department":
         return addDepartment();
 
       case "View All Roles":
         return viewAllRoles();
-      
+
       case "Add Role":
         return addRole();
- 
     }
   });
 }
